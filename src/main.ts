879e84d9b62b917087f84d1814360d430ef70fd5
Manujdixit <manujdixit04@gmail.com>
2025-07-18 17:17:57 +0530

    Add PostgreSQL exception filter and update main application setup
    - Introduced AllExceptionsFilter to handle PostgreSQL exceptions with specific error messages.
    - Updated main.ts to use the new exception filter and configured Swagger documentation.
    - Added new dependencies for @nestjs/swagger and updated existing ones in package.json and package-lock.json.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/postgres-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Corp Backend')
+    .setDescription('API documentation for the Corp Backend')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
